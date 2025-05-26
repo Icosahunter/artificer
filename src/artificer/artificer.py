f50ad9e500d23cc07fa12326b54e5ea6614ca051
Icosahunter <anzelknight@gmail.com>
2025-05-25 21:34:16 -0500

    Add ArtifactLoader and AttrArtifact classes

--- src/artificer/artificer.py
+++ src/artificer/artificer.py
@@ -43,13 +43,13 @@ class Rule:
         return True if rule.target_tags & self.source_tags else False
 
     def __call__(self, artifacts):
-        self.function(artifacts)
+        for artifact in filter(artifacts, self.source_tags):
+            self.function(artifact)
 
 class Artifact:
     def __init__(self, obj=None, tags=[]):
         self.tags = _resolve_tags(tags)
         self.obj = obj
-        self._set_type_tags()
 
     def add_tag(self, tag):
         self.tags.update(_resolve_tag(tag))
@@ -65,34 +65,67 @@ class Artifact:
         tags = ', '.join(tags)
         return f'<{type(self).__name__} obj:{self.obj} tags:{{{tags}}}>'
 
-    def _set_type_tags(self):
-        self.tags.add(f'py:{type(self.obj).__name__}')
-        if isinstance(self.obj, Mapping):
-            self.tags.add('std:mapping')
-            self.tags.update((f'std:mapping.keys.{x}' for x in self.obj.keys()))
-        if isinstance(self.obj, Sequence):
-            self.tags.add('std:sequence')
-        if isinstance(self.obj, str) and urlparse(self.obj).scheme:
-            self.tags.add('std:url')
-        if isinstance(self.obj, Path) or (isinstance(self.obj, str) and _str_is_pathlike(self.obj)):
-            self.tags.add('std:path')
-            # If dir/file not specified by tags argument, make try to figure it out
-            if not ('std:path.dir' in self.tags or 'std:path.file' in self.tags):
-                path = Path(self.obj)
+class AttrArtifact(Artifact):
+
+    class AttrArtifactDescriptor:
+        def __init__(self, obj, attr):
+            self.obj = obj
+            self.attr = attr
+
+        def __get__(self, obj, objtype=None):
+            return self.obj[self.attr]
+
+        def __set__(self, obj, value):
+            self.obj[self.attr] = value
+
+    def __init__(self, obj=None, attr='', tags=[]):
+        super().__init__(self.AttrArtifactDescriptor(obj, attr), tags)
+
+class DefaultArtifactLoader:
+
+    def supports_obj(self, obj):
+        return True
+
+    def load(self, obj):
+        artifact = Artifact(obj)
+        self._set_type_tags(artifact)
+        return [artifact]
+
+    def _set_type_tags(self, artifact):
+        artifact.tags.add(f'py:{type(artifact.obj).__name__}')
+        if isinstance(artifact.obj, Mapping):
+            artifact.tags.add('std:mapping')
+            artifact.tags.update((f'std:mapping.keys.{x}' for x in artifact.obj.keys()))
+        if isinstance(artifact.obj, Sequence):
+            artifact.tags.add('std:sequence')
+        if isinstance(artifact.obj, str) and urlparse(artifact.obj).scheme:
+            artifact.tags.add('std:url')
+        if isinstance(artifact.obj, Path) or (isinstance(artifact.obj, str) and _str_is_pathlike(artifact.obj)):
+            artifact.tags.add('std:path')
+            # If dir/file not specified by tags argument, try to figure it out
+            if not ('std:path.dir' in artifact.tags or 'std:path.file' in artifact.tags):
+                path = Path(artifact.obj)
                 if path.exists():
                     if path.is_dir():
-                        self.tags.add('std:path.dir')
+                        artifact.tags.add('std:path.dir')
                     else:
-                        self.tags.add('std:path.file')
+                        artifact.tags.add('std:path.file')
                 elif path.suffix:
-                    self.tags.add('std:path.file')
-                elif isinstance(self.obj, str):
-                    if self.obj[-1] in ['/', '\\']:
-                        self.tags.add('std:path.dir')
+                    artifact.tags.add('std:path.file')
+                elif isinstance(artifact.obj, str):
+                    if artifact.obj[-1] in ['/', '\\']:
+                        artifact.tags.add('std:path.dir')
 
 class Artificer:
-    def __init__(self, rules):
+
+    default_artifact_loaders = [DefaultArtifactLoader()]
+
+    def __init__(self, rules, artifact_loaders = None):
         self.rules = rules
+        if artifact_loaders:
+            self.artifact_loaders = artifact_loaders
+        else:
+            self.artifact_loaders = Artificer.default_artifact_loaders
         self._build_dep_graph()
 
     def _build_dep_graph(self):
@@ -105,6 +138,15 @@ class Artificer:
             self.dep_graph[rule1] = rule1_deps
         self.build_steps = toposort_flatten(self.dep_graph, False)
 
-    def build(self, artifacts):
+    def _load_artifacts(self, objs):
+        artifacts = []
+        for obj in objs:
+            for loader in self.artifact_loaders:
+                if loader.supports_obj(obj):
+                    artifacts += loader.load(obj)
+                    break
+
+    def build(self, objs):
+        artifacts = self._load_artifacts(objs)
         for step in self.build_steps:
             step(artifacts)
